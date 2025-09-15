ccd6df947ed11dc5d2b13c036bf31a5f6c4e5cd2
mariapauli22 <maria.varelao@campusucc.edu.co>
2025-09-15 14:33:25 +0000

    .

---
+++ Clase Sep 15/ValidacionCadenasPila.java
@@ -0,0 +1,34 @@
+public class ValidacionCadenasPila{
+    Public static void main (String[] args){
+
+        String[] llaves = {"{","{","}","}"};
+
+        //Cree una pila de llaves y verifique si las llaves estan
+        //Completas,ed decir, una llave que abre riene una llave que cierra
+
+        Stack <String> cadenas = new Stack <>();
+
+        for (int i=0; i <llaves.length; i ++) {
+            p.push (llaves [i]);
+
+        }
+
+        system.out.println(p);
+
+        for (int i=0; i <p.size (); i ++) {
+            if (p.peek().equals(anObject:"{")){
+                contador1 += 1;
+            }else{
+                contador2 += 1;
+            }
+            p.pop ();
+        
+        }
+
+        system.out.println( contador1== contador2? "Equilibrados": "No equilibrados");
+
+
+
+
+    }
+}
